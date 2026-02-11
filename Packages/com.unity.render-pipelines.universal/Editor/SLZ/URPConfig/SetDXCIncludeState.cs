fa82f19b855c52291ea8ae6d23b1f605d84421bc
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-02-10 19:08:23 -0800

    Fix out of date SetDXCIncludeState, add texture post-processor source code to repo, add linux version of texture post-processor, and start new native rendering plugin to replace the old NvAPI shading rate plugin.

---
+++ Packages/com.unity.render-pipelines.universal/Editor/SLZ/URPConfig/SetDXCIncludeState.cs
@@ -0,0 +1,42 @@
+using SLZ.SLZEditorTools;
+using System.IO;
+using UnityEngine;
+
+namespace SLZ.DXCUpdater
+{
+    public static class SetDXCIncludeState
+    {
+        static string packageName = "com.stresslevelzero.urpconfig";
+        public static bool Set(bool patched, uint major, uint minor, uint patch, uint build)
+        {
+            //Debug.Log($"Setting DXCUpdateState");
+            URPConfigManager.Initialize();
+            string includePath = Path.Combine(URPConfigManager.packagePath, "include", "DXCUpdateState.hlsl");
+            //Debug.Log($"DXCUpdateState path: {includePath}");
+            if (!File.Exists(includePath))
+            {
+                Debug.LogError($"Critical shader include file is missing ({includePath})");
+                return false;
+            }
+
+            string comment = patched ? "" : "//";
+            string file =
+                $"#ifndef SLZ_DXC_STATE\n" +
+                $"\t#define SLZ_DXC_STATE\n" +
+                $"\t{comment}#define SLZ_DXC_UPDATED\n" +
+                $"\t{comment}#define SLZ_DXC_VERSION_MAJOR {major}\n" +
+                $"\t{comment}#define SLZ_DXC_VERSION_MINOR {minor}\n" +
+                $"\t{comment}#define SLZ_DXC_VERSION_PATCH {patch}\n" +
+                $"\t{comment}#define SLZ_DXC_VERSION_BUILD {build}\n" +
+                $"#endif";
+            string original = File.ReadAllText(includePath);
+            if (!string.Equals(original, file, System.StringComparison.InvariantCulture))
+            {
+                Debug.Log($"DXCUpdateState needs to be updated!");
+                File.WriteAllText(includePath, file);
+            }
+
+            return true;
+        }
+    }
+}
