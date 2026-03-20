b8b89ab50fd9be8fe7a0ecdaef5a2cf999291e65
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-03-20 12:42:35 -0700

    Working on lighting model, add check for HLSL 2021 support

--- Packages/com.unity.render-pipelines.universal/ShaderLibrary/SLZ/HLSL2021Support.hlsl
+++ Packages/com.unity.render-pipelines.universal/ShaderLibrary/SLZ/HLSL2021Support.hlsl
@@ -1,8 +1,20 @@
 #ifndef SLZ_HLSL2021_SUPPORT
 #define SLZ_HLSL2021_SUPPORT
 
-#if !defined(UNITY_COMPILER_DXC) || (SLZ_DXC_VERSION_MINOR < 8 && SLZ_DXC_VERSION_MAJOR <= 1)
+// Currently, the only way to get HLSL 2021 is to use our modified version of the DXC compiler that forces -HV2021
+// Unfortunately, unity's shader preprocessor resolves all preprocessor macros before they get to the compiler,
+// so we can't check the predefined macros for hlsl version :(
+// unity uses a fork of the compiler off of version 1.7, ours is version 1.8.0
+#if defined(UNITY_COMPILER_DXC) && (SLZ_DXC_VERSION >= 0x00408000)
+    #define HLSL_2021
+#endif 
+
+// Define HLSL2021's vector logic functions to equivalents for non-HLSL2021. These were introduced to allow short-circuiting
+// ternary operations on non-vector conditions.
+#if !defined(HLSL_2021)
     #define select(a, b, c) ((a) ? (b) : (c))
+    #define and(a, b) ((a) && (b))
+    #define or(a, b) ((a) || (b))
 #endif
 
 #endif // SLZ_HLSL2021_SUPPORT
--- Packages/com.unity.render-pipelines.universal/ShaderLibrary/SLZ/Templates/PhysicallyBasedLighting.hlsl
+++ Packages/com.unity.render-pipelines.universal/ShaderLibrary/SLZ/Templates/PhysicallyBasedLighting.hlsl
@@ -8,28 +8,30 @@
 half4 PhysicallyBasedLighting(MESH_DATA meshData, PHYS_DATA physData, SPECULAR_MODEL specularModel)
 {
 
-    // handle alpha premultiplication for transparent surfaces. diffuse lighting is darkened by alpha, but specular is unaffected.
-    // Instead, reflectivity reduces transmission of light 
+    // handle alpha premultiplication for transparent surfaces. diffuse lighting is multiplied by alpha, but specular is unaffected.
+    // Instead, reflectivity reduces transmission of light.  
     if (physData.surfaceType == min16int(1))
     {
         physData.albedo *= physData.alpha;
         physData.emission *= physData.alpha;
 
-
-        half monoReflectivity = max(physData.specularColor.r, max(physData.specularColor.g, physData.specularColor.b));
+        // increase the alpha to 1 as the specular reflectance goes to 1
+        half monoReflectance = max(physData.reflectance.r, max(physData.reflectance.g, physData.reflectance.b));
+        // inaccurate pow4 fresnel, but good enough
         half fresnelTerm = (half(1.0h) - saturate(meshData.NoV));
         fresnelTerm *= fresnelTerm;
         fresnelTerm *= fresnelTerm;
-        physData.alpha = lerp(monoReflectivity, 1, fresnelTerm);
+        physData.alpha = saturate(physData.alpha + lerp(monoReflectance, 1, fresnelTerm));
     }
 
     half3 diffuse = meshData.vertexLighting * physData.albedo;
 
-    half3 specular = half3(0.0h, 0.0h, 0.0h);
+    half3 reflectionDir = SPECULAR_MODEL::CalculateReflectionVector(meshData, physData);
+    half3 specular = GlossyEnvironmentReflection(reflectionDir, meshData.position, physData.perceptualRoughness, 1.0f, meshData.screenUV);
 
+    //specular += SPECULAR_MODEL::CalculatePunctualSpecular(meshData, physData, half3(0, 1, 0));
 
-
-    return half4(SPECULAR_MODEL::CalculatePunctualSpecular(meshData, physData, half3(0, 1, 0)), 1);
+    return half4(specular, 1);
 }
 
 #endif // SLZ_PBR_LIGHTING_TEMPLATE
