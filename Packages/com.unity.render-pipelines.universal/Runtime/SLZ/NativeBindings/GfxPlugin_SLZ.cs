7d89bf8bb7f448553a151b519aa00e6bfdb13f93
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2026-08-26 13:11:43 -0700

    Update to GfxPlugin_SLZ missing from a previous commit

--- Packages/com.unity.render-pipelines.universal/Runtime/SLZ/NativeBindings/GfxPlugin_SLZ.cs
+++ Packages/com.unity.render-pipelines.universal/Runtime/SLZ/NativeBindings/GfxPlugin_SLZ.cs
@@ -7,7 +7,26 @@ namespace SLZ
     public class GfxPlugin_SLZ
     {
         const string pluginName = "GfxPlugin_SLZ";
-        [DllImport(pluginName)]
-        public static extern UInt32 SupportsLayeredShadingRate();
+        #if !UNITY_ANDROID
+        [DllImport(pluginName, EntryPoint="SupportsLayeredShadingRate")]
+        public static extern UInt32 SupportsLayeredShadingRate_Extern();
+        #endif
+        private static bool supportsLayeredShadingRate_cached = false;
+        private static bool SupportsLayeredShadingRate_value = false;
+
+        public static bool SupportsLayeredShadingRate()
+        {
+            #if !UNITY_ANDROID
+            if (!supportsLayeredShadingRate_cached)
+            {
+                SupportsLayeredShadingRate_value = SupportsLayeredShadingRate_Extern() != 0;
+                supportsLayeredShadingRate_cached = true;
+            }
+            return SupportsLayeredShadingRate_value;
+            #else
+            return true;
+            #endif
+        }
+
     }
 }
